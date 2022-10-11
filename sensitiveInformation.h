7ad70821871f351d35b013acb224c731605e1cb7
4060269 <98860251+4060269@users.noreply.github.com>
2022-10-11 11:55:47 +1100

    Add files via upload

---
+++ sensitiveInformation.h
@@ -0,0 +1,17 @@
+/*
+ * Contains any sensitive Infomration that you do not want published to Github.
+ * 
+ * The SSID and Password variables will need to be changed if you’re connecting to another Wireless Access Point (such as at home).
+ * The `http_username` and `http_password` variables are used to authenticate when users are attempting to access secured pages.
+ * 
+ * Make sure this file is included in the .gitignore!
+ * 
+ */
+
+const char* host            = "RMS";
+const char* ssid            = "Eyes";        // Wifi Network Name
+const char* password        = "Password01";       // Wifi Password
+
+
+const char* http_username   = "admin";
+const char* http_password   = "admin";
